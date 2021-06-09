6a4cf2a17370ae8ab6c1ce705e9f4b80a0728681
PHYTAX231 <85590342+PHYTAX231@users.noreply.github.com>
2021-06-09 12:56:26 +0700

    Add files via upload

---
+++ KonsepOOP/src/konsepoop/KonsepOOP.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konsepoop;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class KonsepOOP {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        oop oke = new oop();
+        System.out.println("Namaku: "+oke.nama);
+        System.out.println("NIS: "+oke.NIS);
+    }
+    
+}
